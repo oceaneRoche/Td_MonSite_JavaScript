5f47232dfff481ff78a184acdec8c89c0dea4021
Oceane <email@gmail.com>
2024-04-04 09:34:38 +0200

    init

---
+++ javaScript/index.js
@@ -0,0 +1,45 @@
+let compte = {
+    "login" : "btonnot",
+    "password" : "btonnot",
+    "nom" : "tonnot",
+    "prenom" : "bob"
+}
+
+document.onreadystatechange = function () {
+    if (document.readyState == 'interactive') {
+        $(".dropdown-trigger").dropdown();
+        let compteSession = sessionStorage.getItem("compte")
+        let connecte = document.getElementsByClassName("connecte");
+        let deconnecte = document.getElementsByClassName("deconnecte")
+        if(compteSession == null) {
+            console.log("pas de compte")
+            for( let i=0;i<connecte.length;i++){
+                connecte[i].style.display ="none";
+            }
+            for( let i=0;i<connecte.length;i++){
+                deconnecte[i].style.display ="";
+            }
+        } else {
+            console.log("un compte")
+            console.log("compte = " + compteSession)
+            compte = JSON.parse(compteSession)
+
+
+        }
+        document.getElementById("login").oninput = function () {
+            console.log("onchange")
+
+        }
+        document.getElementById("password").oninput = function () {
+            console.log("onchange")
+
+        }
+
+        document.getElementById("submit").onclick = function ( ) {
+            console.log("un clic")
+            if(document.getElementById("login").value === compte.login) {
+
+            }
+        }
+    }
+}
